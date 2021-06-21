e7d9b1488731b5e1b0a8f6bb5e62473ed8d677ca
shivam kumar <20051252@kiit.ac.in>
2021-06-21 15:48:05 +0530

    learning github

---
+++ Shivam/check.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define mod 1e9+7
+#define modulo 998244353
+#define deb(...) logger(#__VA_ARGS__, __VA_ARGS__)
+template<typename ...Args>
+void logger(string vars, Args&&... values) {
+cout << vars << " = ";
+string delim = "";
+(..., (cout << delim << values, delim = ", "));
+}
+
+typedef unsigned long long int  ull;
+typedef long long int ll;
+
+void read_input_from_file()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input_file.txt","r",stdin);
+    freopen("output_file.txt","w",stdout);
+    #endif
+}
+
+void shivam()
+{
+    
+}
+
+int main()
+{
+    auto begin = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // read_input_from_file();
+    ll t = 1;
+    // cin>>t;
+    for(ll i=1;i<=t;i++)
+    {
+        shivam();
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    //cout <<"\nExecution time: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - begin).count() << " seconds "<<"\n";
+    return 0;
+}
