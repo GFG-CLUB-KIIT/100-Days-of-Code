d45e2a1816d06b86b4767087d53b6f3126257538
Lalit Kumar <lalitkumar.jin123@gmail.com>
2021-06-23 04:29:17 +0530

    day_2

---
+++ Lalit/Day_2.cpp.cpp
@@ -0,0 +1,57 @@
+https://leetcode.com/problems/search-a-2d-matrix/
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target)
+    {
+            int n = matrix.size();
+            int m = matrix[0].size();
+
+            bool flag = false;
+
+            for(int i=0; i<n; i++)
+            {
+                int low = 0, high = m-1;
+
+                if(low == high)
+                {
+                    if(matrix[i][low] == target)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+
+                while(low<high)
+                {
+                    int mid = (low + high) / 2 ;
+                    // cout<<matrix[i][mid]<<" ";
+                    if(matrix[i][mid] == target)
+                    {
+                        flag = true;
+                        break;
+                    }
+
+                    if(matrix[i][mid] < target)
+                    {
+                        low = mid + 1;
+                    }
+                    else
+                    {
+                        high = mid;
+                    }
+                }
+                if(flag)
+                {
+                    break;
+                }
+                if(matrix[i][low] == target)
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+        return flag;
+    }
+};
