7f6e1b04882dba6ef328497ef11efe3136a773ff
Snehil <cascadenienuo@gmail.com>
2021-06-27 00:18:05 +0530

    Day 5 and Day 6

---
+++ Snehil/Day-6/False_Number.cpp
@@ -0,0 +1,47 @@
+// https://www.codechef.com/LTIME97B/problems/FALSNUM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define boost ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define int long long
+const int mod = 1e9 + 7;
+
+#define f(i,n) for(int i=0;i<n;i++)
+#define rep(i,s,n) for(int i=s;i<n;i++)
+#define fm(it,mp) for(auto it=mp.begin();it!=mp.end();it++)
+#define pb push_back
+#define fill(a, x) memset(a, x, sizeof(a))
+#define MP make_pair
+#define F first
+#define S second
+#define vi vector<int>
+#define vii vector<pair<int,int>>
+#define vvi vector<vector<int>>
+#define mi map<int,int>
+
+void solve(int tests){
+    string a;
+    cin>>a;
+
+    string s;
+    s+='1';
+    s+=a;
+    if(a[0]=='1'){
+        s[1] = '0';
+    }
+    cout<<s<<endl;
+}
+
+signed main(){
+    boost
+
+    int tests = 1;
+    cin >> tests;
+
+    for(int i=1;i<=tests;i++){
+        solve(i);
+    }
+
+    return 0;
+}
