8a30b4ad9c3029059c2f6eacf04b9885aacbaf7e
NDhar <nittishna.dhar@gmail.com>
2021-06-29 08:30:56 +0530

    added D-8 code

--- Nittishna/D-7.cpp
+++ Nittishna/D-7.cpp
@@ -1,3 +1,5 @@
+// Find the difference between the sum of the squares of the first 
+// one hundred natural numbers and the square of the sum.
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -5,12 +7,12 @@ int main(){
     
     int SumOfSquares = 0, prod = 1, sum = 0, SquareOfSum = 0, diff = 0;
 
-    for(int i = 1; i <= 10; ++i){
+    for(int i = 1; i <= 100; ++i){
         prod = i*i ;
         SumOfSquares += prod;
     }
     cout << "sum of squares: " << SumOfSquares << endl;
-    for (int j = 1; j <= 10; j++){
+    for (int j = 1; j <= 100; j++){
         sum += j;
     } 
    
